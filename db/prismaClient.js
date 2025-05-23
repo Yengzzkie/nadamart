8210d0f55b0fffa6bd4bd29ad8ee8559e3db0b64
Yengzzkie <gatchalian.manuel@gmail.com>
2025-05-23 08:54:29 -0400

    removed "postinstall": "prisma generate"

--- db/prismaClient.js
+++ db/prismaClient.js
@@ -1,10 +1,5 @@
-// lib/prisma.js
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/generated/prisma";
 
-const globalForPrisma = global;
+const prisma = new PrismaClient()
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+export default prisma;
