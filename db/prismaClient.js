a8689ffe679f8fd4b7cbc01be7bdbce72c1de26a
Yengzzkie <gatchalian.manuel@gmail.com>
2025-05-23 08:43:01 -0400

    fixed prismaClient

--- db/prismaClient.js
+++ db/prismaClient.js
@@ -1,5 +1,10 @@
-import { PrismaClient } from "@/generated/prisma";
+// lib/prisma.js
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const globalForPrisma = global;
 
-export default prisma;
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
