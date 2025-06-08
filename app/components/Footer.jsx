edeb5ba47864229a9d7d6ec9c094c6013751a962
Yengzzkie <gatchalian.manuel@gmail.com>
2025-06-08 17:09:21 -0400

    - fixed footer overflowing on mobile screen

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,20 +2,23 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="w-full flex flex-wrap items-center justify-center font-light text-nowrap text-sm p-4 lg:p-8 border-t-[1px]">
+    <footer className="w-full flex flex-wrap items-center justify-center font-light text-nowrap text-sm p-4 border-t-[1px]">
       <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4 font-display"
+        className="flex flex-col items-center gap-2 hover:underline hover:underline-offset-4 font-display"
         href="https://www.linkedin.com/in/manuel-gatchalian/"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Image
-          src="/globe.svg"
-          alt="Globe icon"
-          width={16}
-          height={16}
-        />
-        NadaMart {new Date().getFullYear()} - All Rights Reserved | Developed by <strong>Manuel Gatchalian</strong>
+        <div className="flex items-center gap-2">
+          <Image
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          <p>NadaMart {new Date().getFullYear()} - All Rights Reserved</p>
+        </div>
+        <p>Developed by <strong>Manuel Gatchalian</strong></p>
       </a>
     </footer>
   );
